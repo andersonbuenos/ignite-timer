887299f1bf4cf60761ada0eeffac227a2d0ba4c5
andersonbuenos <andersonbuenos@live.com>
2023-10-24 06:12:09 -0400

    Inseri formulário, criei o cowndown, estilizei os components do styles.ts

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import {ButtonContainer, ButtonVariant} from './Button.styles';
-
-interface ButtonProps {
-    variant?: ButtonVariant;
-}
-
-export function Button({ variant = 'Primary' }) {
-    return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import { Play } from "phosphor-react";
+import { 
+    HomeContainer, 
+    FormContainer, 
+    CountdownContainer, 
+    Separator, 
+    StartCountdownButton,
+    TaskInput,
+    MinutesAmountInput,
+} from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer> 
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em </label>
+                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput id="minutesAmount" />
+
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span> 
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton disabled type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
