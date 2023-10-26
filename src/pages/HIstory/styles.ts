ef0625aaa81d1133c9bffdde9e01eb5aeb0f2d32
andersonbuenos <andersonbuenos@live.com>
2023-10-25 22:42:27 -0400

    Inserido um objeto que aponte o status concluído e demais com uma cor padrão para casa

--- src/pages/HIstory/styles.ts
+++ src/pages/HIstory/styles.ts
@@ -62,3 +62,22 @@ export const HistoryList = styled.div`
 
   }
 `
+
+interface StatusProps {
+  statusColor: 'yellow' | 'red' | 'green';
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme['yellow-500']};
+  }
+  
+`
